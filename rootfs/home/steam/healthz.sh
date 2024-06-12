09365f133c08ed275df28e073c13f6351d12a0a9
João Pedro Barboza Garcia - JGA <joao.garcia@iteris.com.br>
2024-06-12 18:29:33 -0300

    chore(health): netcat ports

--- rootfs/home/steam/healthz.sh
+++ rootfs/home/steam/healthz.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
-# Path to the Saves directory
-SAVES_DIR="/vrising/data/Saves"
+set -eux
 
-# Get the current time in seconds since the epoch
-NOW=$(date +%s)
+echo "Check game port availability"
+nc -nzuv "127.0.0.1" ${VR_GAME_PORT:-"9876"}
 
-# Find the last modified time of files in the Saves directory (in seconds since the epoch)
-LAST_MODIFIED=$(find "$SAVES_DIR" -type f -printf '%T@\n' | sort -rn | head -n 1)
-
-# Convert the LAST_MODIFIED to an integer (strip off the fractional part)
-LAST_MODIFIED_INT=$(printf "%.0f" "$LAST_MODIFIED")
-
-# Calculate the threshold time (last modified time + 180 seconds)
-LAST_MODIFIED_TIME=$(($LAST_MODIFIED_INT + 180))
-
-# Check if the threshold time is less than the current time
-if [ "$LAST_MODIFIED_TIME" -lt "$NOW" ]; then
-    echo "No files updated in the last 3 minutes"
-    exit 1
-else
-    echo "Files updated in the last 3 minutes"
-    exit 0
-fi
+echo "Check query port availability"
+nc -nzuv "127.0.0.1" ${VR_QUERY_PORT:-"9877"}
